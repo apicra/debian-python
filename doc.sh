3594a13e3ce7a52a92546c80a35d4ec158289dd5
tom <tom@sapletta.com>
2020-05-03 12:52:36 +0200

    update filenames

---
+++ doc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set URL=https://docs.gunicorn.org/en/0.17.0/configure.html
+set URL=https://docs.gunicorn.org/en/latest/
+::set BROWSER=chrome
+set BROWSER=firefox
+start %BROWSER% %URL%
+::explorer %URL%
